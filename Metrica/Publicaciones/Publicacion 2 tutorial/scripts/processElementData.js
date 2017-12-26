5e82e40fd695c36443aaa74f727fdfd189ac14e6
Santiago Sañudo Martinez <santisanudo@gmail.com>
2017-12-26 14:30:20 +0100

    Tutorial EPF Composer completado

---
+++ Metrica/Publicaciones/Publicacion 2 tutorial/scripts/processElementData.js
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_jhPCkeo3EeeDIbFNrMXtigActivity"]="./../../sm_plug-in/deliveryprocesses/resources/Sm DP_8922EBE3_8922ebe3_Activity.jpeg"
+contentPage.processPage.imageFiles["_jhPCkeo3EeeDIbFNrMXtig,_n6F5Meo3EeeDIbFNrMXtig,_vHxn4eo3EeeDIbFNrMXtigWPDependency"]="./../../sm_plug-in/deliveryprocesses/resources/Requirements_C5D950D1_e7e831a3_WPDependency.jpeg"
+contentPage.processPage.elementUrls["_4b9wEeo3EeeDIbFNrMXtig"] = ["Use-Case Model","sm_plug-in/deliveryprocesses/use_case_model_A6C1D564.html"];
+contentPage.processPage.elementUrls["_jhPCkeo3EeeDIbFNrMXtig"] = ["Sm DP","sm_plug-in/deliveryprocesses/Sm DP_8922EBE3.html"];
+contentPage.processPage.elementUrls["_R9kQIeo6EeeDIbFNrMXtig"] = ["Descriptor de productos de trabajo nuevo","sm_plug-in/deliveryprocesses/Descriptor de productos de trabajo nuevo_3C198584.html"];
+contentPage.processPage.elementUrls["_n6F5Meo3EeeDIbFNrMXtig"] = ["Inception","sm_plug-in/deliveryprocesses/Inception_D89DAB47.html"];
+contentPage.processPage.elementUrls["_vHxn4eo3EeeDIbFNrMXtig"] = ["Requirements","sm_plug-in/deliveryprocesses/Requirements_C5D950D1.html"];
+contentPage.processPage.elementUrls["_NgJ-weo8EeeDIbFNrMXtig"] = ["Sm Extend and Replace Tester","sm_plug-in/deliveryprocesses/sm_extend_and_replace_tester_A399CCE7.html"];
+contentPage.processPage.elementUrls["_HcFD0eo5EeeDIbFNrMXtig"] = ["Elaboration Iteration 1","sm_plug-in/deliveryprocesses/Elaboration Iteration 1_93D310A8.html"];
+contentPage.processPage.elementUrls["_5Nlk0Oo4EeeDIbFNrMXtig"] = ["Plan and Manage Iteration","sm_plug-in/deliveryprocesses/plan_manage_iteration_FDE04B70.html"];
+contentPage.processPage.elementUrls["_5Xrxceo3EeeDIbFNrMXtig"] = ["Test Case","sm_plug-in/deliveryprocesses/test_case_19199583.html"];
+contentPage.processPage.elementUrls["_sdUh4Oo5EeeDIbFNrMXtig"] = ["Construction","sm_plug-in/deliveryprocesses/Fase nuevo_DB0221F9.html"];
+contentPage.processPage.elementUrls["_5VcVwuo3EeeDIbFNrMXtig"] = ["Sm Extend and Replace Tester","sm_plug-in/deliveryprocesses/sm_extend_and_replace_tester_82CEA9CA.html"];
+contentPage.processPage.elementUrls["_5Nlk2-o4EeeDIbFNrMXtig"] = ["[Technical Specification]","sm_plug-in/deliveryprocesses/technical_specification_slot_D074938C.html"];
+contentPage.processPage.elementUrls["_5Xs_keo3EeeDIbFNrMXtig"] = ["Test Log","sm_plug-in/deliveryprocesses/test_log_FA6F101.html"];
+contentPage.processPage.elementUrls["_5VcVxOo3EeeDIbFNrMXtig"] = ["[Technical Specification]","sm_plug-in/deliveryprocesses/technical_specification_slot_4E2997C3.html"];
+contentPage.processPage.elementUrls["_EHQdEeo5EeeDIbFNrMXtig"] = ["Elaboration","sm_plug-in/deliveryprocesses/Elaboration_7557F6F0.html"];
+contentPage.processPage.elementUrls["_5Nlk3Oo4EeeDIbFNrMXtig"] = ["Sm Extend and Replace Tester","sm_plug-in/deliveryprocesses/sm_extend_and_replace_tester_4B95F1CD.html"];
+contentPage.processPage.elementUrls["_4bvtouo3EeeDIbFNrMXtig"] = ["Architect","sm_plug-in/deliveryprocesses/architect_B9DFBDEA.html"];
+contentPage.processPage.elementUrls["_5Nmy-eo4EeeDIbFNrMXtig"] = ["Architect","sm_plug-in/deliveryprocesses/architect_C5B6E816.html"];
+contentPage.processPage.elementUrls["_5Nmy8uo4EeeDIbFNrMXtig"] = ["Prepare Environment","sm_plug-in/deliveryprocesses/Prepare Environment_42997F7B.html"];
+contentPage.processPage.elementUrls["_5XuNseo3EeeDIbFNrMXtig"] = ["Test Script","sm_plug-in/deliveryprocesses/test_script_5B2930E6.html"];
+contentPage.processPage.elementUrls["_5Nlk1uo4EeeDIbFNrMXtig"] = ["[Technical Test Results]","sm_plug-in/deliveryprocesses/technical_test_results_slot_BBA921B5.html"];
+contentPage.processPage.elementUrls["_5Nmy-uo4EeeDIbFNrMXtig"] = ["[Technical Architecture]","sm_plug-in/deliveryprocesses/technical_architecture_slot_D054C826.html"];
+contentPage.processPage.elementUrls["_5Nlk3Oo4EeeDIbFNrMXtig_0ZlSsMlgEdmt3adZL5Dmdw"] = ["Test Log","sm_plug-in/deliveryprocesses/test_log_8D2D5587.html"];
