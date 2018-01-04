f786e880207648fb2bc39ed19a94f31bfb375273
Santi <santisanudo@gmail.com>
2018-01-04 12:00:32 +0100

    Practica finalizada

---
+++ Metrica/Publicaciones/Mantenimiento V3 Metrica/scripts/processElementData.js
@@ -0,0 +1,73 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_iXlI8fE0EeefMrLphPE_EQ,_uQmlYfE0EeefMrLphPE_EQActivity"]="./../../mantenimiento_de_sistemas_de_informacion/deliveryprocesses/resources/MSI 1 Registro de la Petición_C625D4A_1ed8d23a_Activity.jpeg"
+contentPage.processPage.imageFiles["_iXlI8fE0EeefMrLphPE_EQ,_wmgFwfE0EeefMrLphPE_EQActivity"]="./../../mantenimiento_de_sistemas_de_informacion/deliveryprocesses/resources/MSI 2 Análisis de la Petición_8E2BA366_a0a21856_Activity.jpeg"
+contentPage.processPage.imageFiles["_iXlI8fE0EeefMrLphPE_EQActivity"]="./../../mantenimiento_de_sistemas_de_informacion/deliveryprocesses/resources/Proceso Mantenimiento MÉTRICA V3_B990FA9C_b990fa9c_Activity.jpeg"
+contentPage.processPage.imageFiles["_iXlI8fE0EeefMrLphPE_EQ,_0WIZMfE0EeefMrLphPE_EQActivity"]="./../../mantenimiento_de_sistemas_de_informacion/deliveryprocesses/resources/MSI 3 Preparación de la Implementación de la Modificación_D6593313_e8cfa803_Activity.jpeg"
+contentPage.processPage.imageFiles["_iXlI8fE0EeefMrLphPE_EQ,_6l5uIPE0EeefMrLphPE_EQActivity"]="./../../mantenimiento_de_sistemas_de_informacion/deliveryprocesses/resources/MSI 4 Seguimiento y Evaluación de los cambios hasta la Aplicació_9D7AA9BF_aff11eaf_Activity.jpeg"
+contentPage.processPage.elementUrls["_f4L49_E1EeefMrLphPE_EQ"] = ["Plan de Pruebas de Regresión","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/plan_de_pruebas_de_regresin_7710AE3F.html"];
+contentPage.processPage.elementUrls["_grgMMfE1EeefMrLphPE_EQ"] = ["MSI 4.2 Realización de las Pruebas de Regresión","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_4.2_realizacin_de_las_pruebas_de_regresin_1E61C26D.html"];
+contentPage.processPage.elementUrls["_grgzRvE1EeefMrLphPE_EQ"] = ["Evaluación del Cambio","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/evaluacin_del_cambio_26CC3C05.html"];
+contentPage.processPage.elementUrls["_e43J6_E1EeefMrLphPE_EQ"] = ["Plan de Mantenimiento (IAS 7.2)","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/plan_de_mantenimiento_ias_7.2_B0ABC492.html"];
+contentPage.processPage.elementUrls["_f4L47fE1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones3.1_2291FC08.html"];
+contentPage.processPage.elementUrls["_eGAwpPE1EeefMrLphPE_EQ"] = ["Petición de Mantenimiento (externo)","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/peticin_de_mantenimiento_externo_40EC1E3B.html"];
+contentPage.processPage.elementUrls["_f4L45_E1EeefMrLphPE_EQ"] = ["Responsable de Mantenimiento","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/responsable_de_mantenimiento_64C925C3.html"];
+contentPage.processPage.elementUrls["_f4L49fE1EeefMrLphPE_EQ"] = ["Plan de Acción para la Modificación","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/plan_de_accin_para_la_modificacin_ABB5C046.html"];
+contentPage.processPage.elementUrls["_f4L44PE1EeefMrLphPE_EQ"] = ["MSI 3.1 Identificación de Elementos Afectados","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_3.1_identificacin_de_elementos_afectados_E6434195.html"];
+contentPage.processPage.elementUrls["_f4L44_E1EeefMrLphPE_EQ"] = ["Equipo de Mantenimiento","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/equipo_de_mantenimiento_A03743A4.html"];
+contentPage.processPage.elementUrls["_0WIZMfE0EeefMrLphPE_EQ"] = ["MSI 3 Preparación de la Implementación de la Modificación","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/MSI 3 Preparación de la Implementación de la Modificación_D6593313.html"];
+contentPage.processPage.elementUrls["_6l5uIPE0EeefMrLphPE_EQ"] = ["MSI 4 Seguimiento y Evaluación de los cambios hasta la Aplicación","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/MSI 4 Seguimiento y Evaluación de los cambios hasta la Aplicació_9D7AA9BF.html"];
+contentPage.processPage.elementUrls["_eGAwovE1EeefMrLphPE_EQ"] = ["Responsable de Mantenimiento","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/responsable_de_mantenimiento_75913042.html"];
+contentPage.processPage.elementUrls["_grgMNfE1EeefMrLphPE_EQ"] = ["Equipo de Mantenimiento","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/equipo_de_mantenimiento_E2F3A48C.html"];
+contentPage.processPage.elementUrls["_grgzQPE1EeefMrLphPE_EQ"] = ["Producto Software en Desarrollo (externo, procedente de los procesos de desarrollo) ","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/producto_software_en_desarrollo_externo_procedente_de_los_proces_690365C0.html"];
+contentPage.processPage.elementUrls["_f4L46fE1EeefMrLphPE_EQ"] = ["Propuesta de Solución","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/propuesta_de_solucin_5E0019E9.html"];
+contentPage.processPage.elementUrls["_e43J8fE1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones2.2_6E749AD7.html"];
+contentPage.processPage.elementUrls["_e43J4_E1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones1.2_27880054.html"];
+contentPage.processPage.elementUrls["_e43J7_E1EeefMrLphPE_EQ"] = ["Propuesta de Solución","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/propuesta_de_solucin_753DA6B1.html"];
+contentPage.processPage.elementUrls["_grgzTvE1EeefMrLphPE_EQ"] = ["Evaluación del Resultado de las Pruebas de Regresión","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/evaluacin_del_resultado_de_las_pruebas_de_regresin_AFF00043.html"];
+contentPage.processPage.elementUrls["_f4L47_E1EeefMrLphPE_EQ"] = ["Análisis Impacto de los Cambios","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/anlisis_impacto_de_los_cambios_EDECEA01.html"];
+contentPage.processPage.elementUrls["_grgMMvE1EeefMrLphPE_EQ"] = ["MSI 4.3 Aprobación y Cierre de la Petición ","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_4.3_aprobacin_y_cierre_de_la_peticin__28FFA27D.html"];
+contentPage.processPage.elementUrls["_e43J6fE1EeefMrLphPE_EQ"] = ["Resultado del Estudio de la Petición","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/resultado_del_estudio_de_la_peticin_E550D699.html"];
+contentPage.processPage.elementUrls["_eGAwpvE1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones1.1_3A231261.html"];
+contentPage.processPage.elementUrls["_grgMN_E1EeefMrLphPE_EQ"] = ["Responsable de Mantenimiento","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/responsable_de_mantenimiento_AE4E9285.html"];
+contentPage.processPage.elementUrls["_grgzSPE1EeefMrLphPE_EQ"] = ["Plan de Pruebas de Regresión","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/plan_de_pruebas_de_regresin_F22729FE.html"];
+contentPage.processPage.elementUrls["_e42i0fE1EeefMrLphPE_EQ"] = ["MSI 2.2 Estudio de la Propuesta de Solución","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_2.2_estudio_de_la_propuesta_de_solucin_49E589DF.html"];
+contentPage.processPage.elementUrls["_grgzUvE1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones4.3_7481E262.html"];
+contentPage.processPage.elementUrls["_f4L46_E1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones2.2_295B07E2.html"];
+contentPage.processPage.elementUrls["_f4L44fE1EeefMrLphPE_EQ"] = ["MSI 3.2 Establecimiento de Plan de Acción","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_3.2_establecimiento_de_plan_de_accin_D4DC55AB.html"];
+contentPage.processPage.elementUrls["_grgMOfE1EeefMrLphPE_EQ"] = ["Directores de los Usuarios","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/directores_de_los_usuarios_A78586AB.html"];
+contentPage.processPage.elementUrls["_f4L44vE1EeefMrLphPE_EQ"] = ["MSI 3.3 Especificación del Plan de Pruebas de Regresión","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_3.3_especificacin_del_plan_de_pruebas_de_regresin_DF7A35BB.html"];
+contentPage.processPage.elementUrls["_f4L45fE1EeefMrLphPE_EQ"] = ["Jefe de Proyecto","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/jefe_de_proyecto_996E37CA.html"];
+contentPage.processPage.elementUrls["_grgzTPE1EeefMrLphPE_EQ"] = ["Resultado de las Pruebas de Regresión ","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/resultado_de_las_pruebas_de_regresin__B6B90C1D.html"];
+contentPage.processPage.elementUrls["_iXlI8fE0EeefMrLphPE_EQ"] = ["Proceso Mantenimiento MÉTRICA V3","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/Proceso Mantenimiento MÉTRICA V3_B990FA9C.html"];
+contentPage.processPage.elementUrls["_e42i0PE1EeefMrLphPE_EQ"] = ["MSI 2.1 Verificación y Estudio de la Petición","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_2.1_verificacin_y_estudio_de_la_peticin_5B4C75C9.html"];
+contentPage.processPage.elementUrls["_eGAwofE1EeefMrLphPE_EQ"] = ["MSI 1.2 Asignación de la Petición","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_1.2_asignacin_de_la_peticin_6AF35032.html"];
+contentPage.processPage.elementUrls["_uQmlYfE0EeefMrLphPE_EQ"] = ["MSI 1 Registro de la Petición","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/MSI 1 Registro de la Petición_C625D4A.html"];
+contentPage.processPage.elementUrls["_eGAwqPE1EeefMrLphPE_EQ"] = ["Plan de Mantenimiento (IAS 7.2)","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/plan_de_mantenimiento_ias_7.2_57E005A.html"];
+contentPage.processPage.elementUrls["_e43J5fE1EeefMrLphPE_EQ"] = ["Acuerdo de Nivel de Servicio(IAS 8.3)","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/acuerdo_de_nivel_de_servicioias_8.3_20BEF47A.html"];
+contentPage.processPage.elementUrls["_f4L48fE1EeefMrLphPE_EQ"] = ["Plan de Mantenimiento (IAS 7.2)","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/plan_de_mantenimiento_ias_7.2_E723DE27.html"];
+contentPage.processPage.elementUrls["_e43J4fE1EeefMrLphPE_EQ"] = ["Responsable de Mantenimiento","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/responsable_de_mantenimiento_5C2D125B.html"];
+contentPage.processPage.elementUrls["_grgzRPE1EeefMrLphPE_EQ"] = ["Plan de Acción para la Modificación","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/plan_de_accin_para_la_modificacin_2D9547DF.html"];
+contentPage.processPage.elementUrls["_wmgFwfE0EeefMrLphPE_EQ"] = ["MSI 2 Análisis de la Petición","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/MSI 2 Análisis de la Petición_8E2BA366.html"];
+contentPage.processPage.elementUrls["_eGAwqvE1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones1.2_FEB4F480.html"];
+contentPage.processPage.elementUrls["_e43J7fE1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones1.1_A9E2B8B8.html"];
+contentPage.processPage.elementUrls["_grgzUPE1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones3.2_7B4AEE3C.html"];
+contentPage.processPage.elementUrls["_f4L48_E1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones3.2_B27ECC20.html"];
+contentPage.processPage.elementUrls["_eGAwoPE1EeefMrLphPE_EQ"] = ["MSI 1.1 Registro de la Petición","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_1.1_registro_de_la_peticin_7C5A3C1C.html"];
+contentPage.processPage.elementUrls["_grgzSvE1EeefMrLphPE_EQ"] = ["Producto Software en Desarrollo (externo) ","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/producto_software_en_desarrollo_externo_EB5E1E24.html"];
+contentPage.processPage.elementUrls["_grgzQvE1EeefMrLphPE_EQ"] = ["Análisis Impacto de los Cambios","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/anlisis_impacto_de_los_cambios_623A59E6.html"];
+contentPage.processPage.elementUrls["_e42i0vE1EeefMrLphPE_EQ"] = ["Equipo de Mantenimiento","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/equipo_de_mantenimiento_548369EF.html"];
+contentPage.processPage.elementUrls["_grgMMPE1EeefMrLphPE_EQ"] = ["MSI 4.1 Seguimiento de los Cambios","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/msi_4.1_seguimiento_de_los_cambios_2FC8AE57.html"];
+contentPage.processPage.elementUrls["_e43J5_E1EeefMrLphPE_EQ"] = ["Catálogo de Peticiones","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/catlogo_de_peticiones2.1_EC19E273.html"];
+contentPage.processPage.elementUrls["_grgMM_E1EeefMrLphPE_EQ"] = ["Jefe de Proyecto","mantenimiento_de_sistemas_de_informacion/deliveryprocesses/jefe_de_proyecto_E9BCB066.html"];
